69528e06a5b1423f964c07ab9dba34ebfa96c228
Bulat.V <fclm.man@gmail.com>
2022-11-10 22:49:29 +0400

    init qr module
    init qr module and qr page comonent

---
+++ qr-generator/src/app/qr/qr-page/qr-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qr-page',
+  templateUrl: './qr-page.component.html',
+  styleUrls: ['./qr-page.component.scss']
+})
+export class QrPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ qr-generator/src/app/qr/qr.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QrPageComponent } from './qr-page/qr-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    QrPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class QrModule { }
